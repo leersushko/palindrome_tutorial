358659a1e510941dcf917de47fad0576398c18f1
Lee Sushko <leesushko@gmail.com>
2020-08-04 13:53:45 -0400

    Define letters method, green

--- lib/jsager_palindrome.rb
+++ lib/jsager_palindrome.rb
@@ -14,12 +14,19 @@ class String
 
 	# Returns the letters in the string.
 	def letters
+		the_letters = [] # empty array
+		for i in 0..self.length - 1 do # range is 0 to char count - 1
+			if self[i].match(/[a-zA-Z]/) # regex
+				the_letters << self[i] # inherit
+			end
+		end
+		the_letters.join
 	end
 
 	private
 
 	# Returns content for palindrome testing.
 	def processed_content
-		self.downcase
+		self.letters.downcase
 	end
 end
