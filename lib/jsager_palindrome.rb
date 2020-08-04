1427a89032e700b0fea70c51592f83e2c79729cc
Lee Sushko <leesushko@gmail.com>
2020-08-03 22:19:59 -0400

    Initiliaze repository

---
+++ lib/jsager_palindrome.rb
@@ -0,0 +1,6 @@
+require "jsager_palindrome/version"
+
+module JsagerPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jsager_palindrome/version.rb
@@ -0,0 +1,3 @@
+module JsagerPalindrome
+  VERSION = "0.1.0"
+end
