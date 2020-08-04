05bd23ce152f02f68d23af2af9f255003143484b
Lee Sushko <leesushko@gmail.com>
2020-08-03 23:28:07 -0400

    Add palindrome code, 4 tests, 2 skips

--- lib/jsager_palindrome.rb
+++ lib/jsager_palindrome.rb
@@ -4,3 +4,18 @@ module JsagerPalindrome
   class Error < StandardError; end
   # Your code goes here...
 end
+
+class String
+
+	# Returns true for a palindrome, false otherwise.
+	def palindrome?
+		processed_content == processed_content.reverse
+	end
+
+	private
+
+	# Returns content for palindrome testing.
+	def processed_content
+		self.downcase
+	end
+end
--- test/jsager_palindrome_test.rb
+++ test/jsager_palindrome_test.rb
@@ -8,4 +8,21 @@ class JsagerPalindromeTest < Minitest::Test
   def test_it_does_something_useful
     assert true
   end
+  
+  def test_non_palindrome
+    assert !"apple".palindrome?
+  end
+
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+  
+  def test_mixed_case_palindrome
+    skip
+  end
+  
+  def test_palindrome_with_punctuation
+    skip
+  end
+  
 end
